f12211c4b941e4629f2c6ebbc169f020c83cdce9
tiagorochatech <143637797+tiagorochatech@users.noreply.github.com>
2023-08-30 13:38:20 -0300

    Update get_chamados.php

--- get_chamados.php
+++ get_chamados.php
@@ -1,17 +1,33 @@
-<?php
-$conn = new mysqli('sql201.infinityfree.com', 'if0_34821246', 'YMpCsNw3yL9Z8bP', 'if0_34821246_sistema');
-$query = "SELECT * FROM chamados";
-$result = $conn->query($query);
-
-while ($row = $result->fetch_assoc()) {
-    echo "<tr>";
-    echo "<td>" . $row['nome'] . "</td>";
-    echo "<td>" . $row['tipo'] . "</td>";
-    echo "<td>" . ($row['prioridade'] === 'P' ? 'Prioridade' : 'Normal') . "</td>";
-    echo "<td>" . $row['status'] . "</td>";
-    echo "<td>" . $row['codigo'] . "</td>";
-    echo "</tr>";
-}
-
-$conn->close();
-?>
+<?php
+
+$conn = new mysqli('localhost', 'data_base_name', 'senha', 'banco_criado');
+$query = "SELECT * FROM chamados";
+
+$result = $conn->query($query);
+
+
+
+while ($row = $result->fetch_assoc()) {
+
+    echo "<tr>";
+
+    echo "<td>" . $row['nome'] . "</td>";
+
+    echo "<td>" . $row['tipo'] . "</td>";
+
+    echo "<td>" . ($row['prioridade'] === 'P' ? 'Prioridade' : 'Normal') . "</td>";
+
+    echo "<td>" . $row['status'] . "</td>";
+
+    echo "<td>" . $row['codigo'] . "</td>";
+
+    echo "</tr>";
+
+}
+
+
+
+$conn->close();
+
+?>
+
